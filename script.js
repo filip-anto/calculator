d58a873fb38f300e068a154a1643eb3433f8203c
filip-anto <filip_anto@hotmail.com>
2022-10-24 11:47:32 +0300

    Code Cleanup

--- script.js
+++ script.js
@@ -30,63 +30,48 @@ operate = (a, b, operation) => {
     else if (operation === '*') {
         document.getElementsByClassName("screen")[0].innerHTML = (multiplyNumbers(a, b));
     } else if (operation === '/') {
-        console.log("IM DIVIDED");
         document.getElementsByClassName("screen")[0].innerHTML = (divideNumbers(a, b));
     }
     reset();
 }
 
 function clicked(target) {
-
     if (target.classList.value == "number") {
         if (operator === null) {
-            if (operand1!=null){
-            operand1 = Number(operand1 + target.textContent);
+            (operand1 === null) ? operand1 = Number(target.textContent) : operand1 = Number(operand1 + target.textContent);
             document.getElementsByClassName("screen")[0].innerHTML = operand1;
-        }else{
-            operand1 = Number(target.textContent);
-            document.getElementsByClassName("screen")[0].innerHTML = operand1;
-        }
         } else {
-            if (operand2!=null){
-            operand2 = Number(operand2 + target.textContent);
+            (operand2 === null) ? operand2 = Number(target.textContent) : operand2 = Number(operand2 + target.textContent);
             document.getElementsByClassName("screen")[0].innerHTML = operand2;
-            }else{
-                operand2= Number(target.textContent);
-                document.getElementsByClassName("screen")[0].innerHTML = operand2;
-            }
         }
-    }
-    else if (target.classList.value == "operator") {
+    } else if (target.classList.value == "operator") {
         if (operator === null) {
-            if (operand1 ===null) {
+            if (operand1 === null) {
                 operand1 = Number(document.getElementsByClassName("screen")[0].innerHTML);
             }
-            operator = target.textContent;
         } else if (operand2 != null) {
             operate(operand1, operand2, operator);
-            operator = target.textContent;
-            operand1=Number(document.getElementsByClassName("screen")[0].innerHTML);
+            operand1 = Number(document.getElementsByClassName("screen")[0].innerHTML);
         }
+        operator = target.textContent;
     }
     else if (target.classList.value == "functionality") {
         if (target.textContent === "=") {
             operate(operand1, operand2, operator);
         } else if (target.textContent === "Clear") {
-            operand1 = 0;
+            operand1 = null;
             reset();
             document.getElementsByClassName("screen")[0].innerHTML = 0;
         } else if (target.textContent === ".") {
             (operator === null) ? operand1 = operand1 + "." : operand2 = operand2 + ".";
             document.getElementsByClassName("screen")[0].innerHTML += ".";
         } else if (target.textContent === "+/-") {
-            (operator === null) 
-            ? operand1 = -Number(document.getElementsByClassName("screen")[0].innerHTML) 
-            : operand2 = -operand2;
-            (operator === null) 
-            ? document.getElementsByClassName("screen")[0].innerHTML = -Number(document.getElementsByClassName("screen")[0].innerHTML) 
-            : document.getElementsByClassName("screen")[0].innerHTML = operand2;
+            (operator === null)
+                ? operand1 = -Number(document.getElementsByClassName("screen")[0].innerHTML)
+                : operand2 = -operand2;
+            (operator === null)
+                ? document.getElementsByClassName("screen")[0].innerHTML = -Number(document.getElementsByClassName("screen")[0].innerHTML)
+                : document.getElementsByClassName("screen")[0].innerHTML = operand2;
         }
     }
-    console.table(operand1,operator, operand2)
 }
